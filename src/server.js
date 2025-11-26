5da5638280b3d45dced5b331fcc7b3026aaaf2d7
YusufByn <yusuf.buyukaydin33@gmail.com>
2025-11-26 10:02:40 +0100

    mini commit

--- src/server.js
+++ src/server.js
@@ -2,8 +2,7 @@
 // et de mes fonctions dans le fichier index dans db
 import app from "./app.js";
 import { env } from "./config/env.js";
-import { testConnection, pool } from "./db/index.js";
-
+import { testConnection } from "./db/index.js";
 
 // fonction try and catch pour démarrer le server
 async function start(){
@@ -22,28 +21,5 @@ async function start(){
         process.exit(1);
     }
 }
-
-// // condition 
-// // si -> ce fichier est exécuté directement depuis al ligne de commande alors execute le reste
-// // import meta url = sert a savoir quel fichier est entrain d'etre execute
-// // processargv1 sera remplacé par le fichier execute dcp 
-// // import meta url = url menant vers server.js === file://server.js grossomodo
-// if (import.meta.url === `file://${process.argv[1]}`) {
-//     // fonction pr tester la connexion sql
-//     // si ca fonctionne il execute le then sinon le catch abec l'error qui stop le program avec msg
-//     // d'erreur
-//     // then = quand la promise a reussi execute ca
-//     testConnection.then(()=>{
-//         // console log de test
-//         console.log('test terminé');
-//         // on return ferme la connexion sql
-//         return pool.end();
-//     })
-
-//     .catch((error)=>{
-//         console.error('test manuel echoué', error);
-//         process.exit(1)
-//     })
-// }
 
 start();
